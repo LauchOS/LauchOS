0fca437e7021e131e09bdf8e7afb778dea2ab69d
65Juko <thejulianjuko@gmail.com>
2023-07-22 14:52:24 +0200

    💨 Added command completion, base backspace

--- src/shell/shell.rs
+++ src/shell/shell.rs
@@ -1,9 +1,11 @@
 use crate::{print, println};
+use crate::general::color::Color;
 use crate::shell::command::Command;
 use crate::shell::command_list;
 use crate::shell::command_list::COMMANDS;
 use crate::shell::string;
 use crate::testing::testable::Testable;
+use crate::vga_buffer::screen_char::ColorCode;
 
 pub const BUFFER_LENGTH: usize = 20; // static length for now
 static mut BUFFER: [char; BUFFER_LENGTH] = ['\0'; BUFFER_LENGTH]; // static buffer length until allocs are possible
@@ -16,25 +18,34 @@ pub fn init_shell(){
 
 // Processes Unicode type data, executes commands on enter
 pub fn input_key(char: char){
-    print!("{}", char);
-    if char == '\n'{
-        unsafe {
-            fire_command();
-        }
-        return;
-    }
-    unsafe {
-        BUFFER[POINTER] = char;
-        POINTER += 1;
 
-        if POINTER == BUFFER_LENGTH {
-            // @TODO throw exception at buffer overflow?
-            POINTER = 0;
+    if !char.is_control() {
+        print!("{}", char);
+
+        unsafe {
+            BUFFER[POINTER] = char;
+            POINTER += 1;
+
+            if POINTER == BUFFER_LENGTH {
+                // @TODO throw exception at buffer overflow?
+                POINTER = 0;
+            }
         }
     }
+
+    // handle control characters
+    match char {
+        '\n' => unsafe{ fire_command()}
+        '\u{0008}' => unsafe{ backspace_pressed()}
+        '\t' => unsafe{ tab_pressed()}
+        _ => {}
+    }
+
+
 }
 
 unsafe fn fire_command(){
+    print!("\n");
     let mut success = false;
     for i in 0..COMMANDS.len() {
         if let Some(cmd) = &COMMANDS[i]{
@@ -58,7 +69,6 @@ unsafe fn fire_command(){
                     args[arg_count][pos_in_arg] = BUFFER[j];
                     pos_in_arg += 1;
                 }
-
                 cmd.run(&args);
                 break;
             }
@@ -66,7 +76,11 @@ unsafe fn fire_command(){
     }
 
     if !success {
-        println!("Command not found.")
+        print!("Command '");
+        for i in 0..POINTER {
+            print!("{}", BUFFER[i]);
+        }
+        println!("' not found.")
     }
 
     for i in 0..BUFFER_LENGTH {
@@ -74,8 +88,51 @@ unsafe fn fire_command(){
     }
     POINTER = 0;
     print!("$ ");
-
 }
+
+unsafe fn backspace_pressed(){
+    if POINTER == 0 {return;}
+    POINTER -= 1;
+    BUFFER[POINTER] = '\0';
+    // @TODO remove character from screen
+}
+
+// autocomplete command if possible
+unsafe fn tab_pressed(){
+    let mut char_freq_count = [0; BUFFER_LENGTH];
+    let mut complete_char = ['\0'; BUFFER_LENGTH];
+
+    // Iterate through each command and count character occurrences
+    for i in 0..COMMANDS.len() {
+        if let Some(cmd) = &COMMANDS[i]{
+            for (j, &cmd_char) in cmd.command.iter().enumerate() {
+                if cmd_char == '\0' {
+                    break;
+                }
+                if cmd_char == BUFFER[j] || BUFFER[j] == '\0' {
+                    if cmd_char == complete_char[j] || (complete_char[j] == '\0' && char_freq_count[j] == 0){
+                        complete_char[j] = cmd_char;
+                        char_freq_count[j] += 1;
+                    }else{
+                        complete_char[j] = '\0';
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    for i in 0..complete_char.len() {
+        if complete_char[i] == '\0' {break}
+        if i < POINTER {continue}
+        print!("{}", complete_char[i]);
+        BUFFER[POINTER] = complete_char[i];
+        POINTER += 1;
+    }
+}
+
+
+
 
 
 
