6c05ab11dce5b3ded1c5a34bea3a4e26169de5b8
65Juko <thejulianjuko@gmail.com>
2023-07-24 14:47:19 +0200

    📚 Added docs to shell

--- src/shell/string.rs
+++ src/shell/string.rs
@@ -1,5 +1,6 @@
 use crate::shell::shell::BUFFER_LENGTH;
 
+/// Converts a &str reference to a char array of static size `BUFFER_LENGTH`
 pub fn to_char_array(str: &str) -> [char; BUFFER_LENGTH] {
     let mut arr = ['\0'; BUFFER_LENGTH];  // Initialize array with null characters
     for (i, c) in str.chars().enumerate() {
@@ -9,6 +10,8 @@ pub fn to_char_array(str: &str) -> [char; BUFFER_LENGTH] {
 }
 
 #[allow(dead_code)]
+/// Compares a &str reference char by char with a char array. <br>
+/// Returns _true_ if they are equal.
 pub fn is_equal_to(s: &str, chars: &[char]) -> bool {
     let mut chars_iter = chars.iter();
 
@@ -21,8 +24,9 @@ pub fn is_equal_to(s: &str, chars: &[char]) -> bool {
     true
 }
 
-// compares both char arrays until the first one is empty
-pub fn are_chars_equal(chars1: &[char], chars2: &[char]) -> bool{
+/// Compares the first word of both char arrays. <br>
+/// Returns _true_ if they are equal
+pub fn are_chars_first_word_equal(chars1: &[char], chars2: &[char]) -> bool{
     for i in 0.. chars1.len(){
         if chars1[i] == '\0' && (chars2[i] == ' ' || chars2[i] == '\0'){
             break;
