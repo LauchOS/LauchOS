5cda4c982e68ace87a1cf3950c121e10309c7735
Julian <75746485+65Juko@users.noreply.github.com>
2023-08-15 11:14:38 +0200

    Enhanced keyboard (#43)
    closes #12 
    
    This contains:
    - Processing and handling of modifiers, to allow for key combinations
    - Port to pc_keyboard 0.7.0 for better use of KeyboardLayouts (version
    needed for AnyLayout and more Layout variants)
    - Current Keyboard layout can now be changed via the shell:
    `set-layout`, `list-layouts`

--- src/drivers/keyboard.rs
+++ src/drivers/keyboard.rs
@@ -1,30 +1,61 @@
 use alloc::vec::Vec;
 use futures_util::StreamExt;
-use pc_keyboard::{DecodedKey, HandleControl, Keyboard, layouts, ScancodeSet1};
+use pc_keyboard::{DecodedKey, HandleControl, Keyboard, KeyCode, KeyState, layouts, Modifiers, ScancodeSet1};
+use pc_keyboard::layouts::AnyLayout;
 use crate::multitasking::scancode_stream::SCANCODE_STREAM;
-use crate::print;
 
 static mut LISTENERS: Vec<KeyboardListenerFn> = Vec::new();
 
-pub type KeyboardListenerFn = fn(key: DecodedKey);
+pub type KeyboardListenerFn = fn(key: DecodedKey, modifiers: &Modifiers);
+
+static mut KEYBOARD: Keyboard<AnyLayout, ScancodeSet1> = Keyboard::new(ScancodeSet1::new(), AnyLayout::De105Key(layouts::De105Key), HandleControl::MapLettersToUnicode);
 
 pub async fn start() {
-    handle_keypresses().await;
+    unsafe {
+        handle_keypresses().await;
+    }
 }
 
-async fn handle_keypresses() {
-    let mut keyboard = Keyboard::new(layouts::Us104Key, ScancodeSet1,
-                                     HandleControl::Ignore);
+/// Handles scancode received from `SCANCODE_STREAM`, gives keycode and modifiers to listeners.
+async unsafe fn handle_keypresses() {
+    let mut modifiers = Modifiers{lshift: false, rshift: false, lctrl: false, rctrl: false, numlock: false, capslock: false, alt_gr: false, rctrl2: false };
+
     while let Some(scancode) = SCANCODE_STREAM.lock().next().await {
-        if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
-            if let Some(key) = keyboard.process_keyevent(key_event) {
-                unsafe {
-                    for listener in &LISTENERS {
-                        listener(key);
+        if let Ok(Some(key_event)) = KEYBOARD.add_byte(scancode) {
+            set_modifier(&mut modifiers, key_event.state == KeyState::Down, key_event.code);
+
+                if let Some(key) = KEYBOARD.process_keyevent(key_event.clone()) {
+                    if key_event.state == KeyState::Down || key_event.state == KeyState::SingleShot {
+                        unsafe {
+                            for listener in &LISTENERS {
+                                listener(key, &modifiers);
+                            }
+                        }
                     }
                 }
-            }
+
         }
+    }
+}
+
+/// Sets modifiers according to keycode.
+fn set_modifier(modifiers: &mut Modifiers, pressed: bool, code: KeyCode){
+    match code {
+        KeyCode::LControl => modifiers.lctrl = pressed,
+        KeyCode::RControl => modifiers.rctrl = pressed,
+        KeyCode::LAlt | KeyCode::RAlt2 => modifiers.alt_gr = pressed,
+        KeyCode::LShift => modifiers.lshift = pressed,
+        KeyCode::RShift => modifiers.rshift = pressed,
+        KeyCode::NumpadLock => modifiers.numlock = pressed,
+        KeyCode::CapsLock => modifiers.capslock = pressed,
+        _ => {}
+    }
+}
+
+/// Change layout of `KEYBOARD` to given `AnyLayout`.
+pub fn set_keyboard_layout(layout: AnyLayout){
+    unsafe {
+        KEYBOARD = Keyboard::new(ScancodeSet1::new(), layout, HandleControl::Ignore);
     }
 }
 
--- src/sys_programs/shell/command_list.rs
+++ src/sys_programs/shell/command_list.rs
@@ -1,6 +1,9 @@
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
+use pc_keyboard::layouts;
+use pc_keyboard::layouts::AnyLayout;
 use crate::{print, println};
+use crate::drivers;
 use crate::general::color::Color;
 use crate::io::interactions;
 use super::command::{Command, ExecuteFn};
@@ -14,7 +17,9 @@ pub fn init_commands(){
         ("exit", exit),
         ("clear", clear),
         ("set-color", set_color),
-        ("list-colors", list_colors)
+        ("list-colors", list_colors),
+        ("set-layout", set_layout),
+        ("list-layouts", list_layouts),
     ];
 
     unsafe {
@@ -46,7 +51,8 @@ fn set_color(args: &Vec<String>){
     if args.len() != 2{
         println!("Invalid format for 'set-color'. \n\
         Required format is 'set-color textcolor backcolor'.\n\
-        Example: 'set-color white black'");
+        Example: 'set-color white black'.\n\
+        Get valid colors with 'list-colors'.");
         return;
     }
     if let Some(text_color) = Color::from_str(&args[0]){
@@ -61,6 +67,51 @@ fn set_color(args: &Vec<String>){
         }
     }else{
         println!("Invalid text color. Get valid colors with 'list-colors'.")
+    }
+}
+
+fn set_layout(args: &Vec<String>){
+    if args.len() != 1{
+        println!("Invalid format for 'set-layout'. \n\
+        Required format is 'set-layout language'.\n\
+        Example: 'set-layout DE'. \n\
+        Get valid layouts with 'list-layouts'");
+        return;
+    }
+    let layout_raw: Option<AnyLayout> = match args[0].to_string().to_lowercase().as_str() {
+        "azerty" => Some(AnyLayout::Azerty(layouts::Azerty)),
+        "colemak" => Some(AnyLayout::Colemak(layouts::Colemak)),
+        "dvp" => Some(AnyLayout::DVP104Key(layouts::DVP104Key)),
+        "de" => Some(AnyLayout::De105Key(layouts::De105Key)),
+        "dvorak" => Some(AnyLayout::Dvorak104Key(layouts::Dvorak104Key)),
+        "jis" => Some(AnyLayout::Jis109Key(layouts::Jis109Key)),
+        "uk" => Some(AnyLayout::Uk105Key(layouts::Uk105Key)),
+        "us" => Some(AnyLayout::Us104Key(layouts::Us104Key)),
+        _ => None
+    };
+
+    if let Some(layout) = layout_raw{
+        drivers::keyboard::set_keyboard_layout(layout);
+        println!("New layout set successfully.")
+    }else{
+        println!("Invalid keyboard layout for 'set-layout'. Get valid layouts with 'list-layouts'");
+    }
+}
+
+fn list_layouts(_args: &Vec<String>){
+    const LAYOUTS: [&str; 8] = [
+        "AZERTY",
+        "COLEMAK",
+        "DVP",
+        "DE",
+        "DVORAK",
+        "JIS",
+        "UK",
+        "US"
+    ];
+
+    for layout in LAYOUTS{
+        println!("'{}'", layout);
     }
 }
 
